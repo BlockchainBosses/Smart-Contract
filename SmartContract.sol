7d737fac05497fa38d245ceb3acc21f118670f03
guillaumekyk <66723364+guillaumekyk@users.noreply.github.com>
2020-06-10 11:23:16 +0200

    Update SmartContract.sol

--- SmartContract.sol
+++ SmartContract.sol
@@ -3,25 +3,25 @@ pragma solidity 0.5.1;
 contract PeopleContract {
     uint256 public peopleCount = 0;
 
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
+
     }
     //struct
     struct Person {
-        uint _id;
-        string _firstName;
-        string _lastName;
+        uint id;
+        string firstName;
+        string lastName;
         unit age;
         bool vegan;
     }
     
     // variable
     address public owner;
-    //mapping
+    // mapping
     mapping(uint => Person) public people;
-
-    
+    // modifier
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
 
     
 
@@ -29,13 +29,7 @@ contract PeopleContract {
         owner = msg.sender;
     }
 
-    function addPerson(
-        string memory _firstName,
-        string memory _lastName
-    )
-        public
-        onlyOwner
-    {
+    function addPerson(string memory _firstName, string memory _lastName) public onlyOwner{
         incrementCount()
         people[peopleCount] = Person(peopleCount, _firstName, _lastName);
     }
